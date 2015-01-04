5360880c76883864f625497cf1c0563c07619b60
Filip Procházka <filip@prochazka.su>
2015-01-04 04:03:36 +0100

    use less

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -12,4 +12,31 @@ use Nette,
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
 
+	/**
+	 * @var \WebLoader\Nette\LoaderFactory
+	 * @inject
+	 */
+	public $webLoader;
+
+
+
+	/**
+	 * @return \WebLoader\Nette\JavaScriptLoader
+	 */
+	protected function createComponentJs()
+	{
+		return $this->webLoader->createJavaScriptLoader('default');
+	}
+
+
+
+	/**
+	 * @return \WebLoader\Nette\CssLoader
+	 */
+	protected function createComponentCss()
+	{
+		return $this->webLoader->createCssLoader('default')
+			->setMedia('screen,projection,tv');
+	}
+
 }
