0e9b7da687c2c03ce10c72505617f67325903049
Filip Procházka <filip@prochazka.su>
2015-01-06 20:59:43 +0100

    bootstrap & config refactoring

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,20 +2,22 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$configurator = new Nette\Configurator;
+return call_user_func(function () {
+	$configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
-$configurator->enableDebugger(__DIR__ . '/../log');
+	//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+	$configurator->enableDebugger(__DIR__ . '/../log');
+	$configurator->setTempDirectory(__DIR__ . '/../temp');
 
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+	$loader = $configurator->createRobotLoader()
+		->addDirectory(__DIR__)
+		->register();
 
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->register();
+	$configurator->addConfig(__DIR__ . '/config/config.neon');
+	$configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
-$configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+	$container = $configurator->createContainer();
+	$container->addService('robotLoader', $loader);
 
-$container = $configurator->createContainer();
-
-return $container;
+	return $container;
+});
