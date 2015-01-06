06a444a4e0b79cfe4f0e94a5b970da01d62f97e6
Filip Procházka <filip@prochazka.su>
2015-01-06 20:59:59 +0100

    Smart router

--- app/model/RouterFactory.php
+++ app/model/RouterFactory.php
@@ -21,7 +21,7 @@ class RouterFactory
 
 
 
-	public function __construct($productionMode, Nette\Http\Request $httpRequest)
+	public function __construct($productionMode)
 	{
 		$this->productionMode = (bool) $productionMode;
 	}
@@ -36,9 +36,51 @@ class RouterFactory
 		$router = new RouteList();
 		$flags = $this->productionMode ? Route::SECURED : 0;
 
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default', $flags);
+		$router[] = new Route('[!<path .*?>/][<action>]', [
+			'action' => 'default',
+			NULL => [
+				Route::FILTER_IN => function (array $params) {
+					if (strpos($path = $params['path'], '/') === FALSE) {
+						return NULL;
+					}
+
+					$params[Route::PRESENTER_KEY] = self::path2presenter($path);
+					unset($params['path']);
+
+					return $params;
+				},
+				Route::FILTER_OUT => function (array $params) {
+					if (strpos($presenter = $params[Route::PRESENTER_KEY], ':') === FALSE) {
+						return NULL;
+					}
+
+					$params['path'] =  self::presenter2path($presenter);
+					unset($params[Route::PRESENTER_KEY]);
+
+					return $params;
+				},
+			]
+		], $flags);
+
+		$router[] = new Route('<presenter>/<action>', 'Homepage:default', $flags);
 
 		return $router;
+	}
+
+
+
+	private static function presenter2path($s)
+	{
+		$s = preg_replace('~([^\\/])(?=[A-Z])~', '$1-', strtr($s, ':', '/'));
+		return str_replace('%2F', '/', rawurlencode(strtolower($s)));
+	}
+
+
+
+	private static function path2presenter($s)
+	{
+		$s = preg_replace('#([\\/-])(?=[a-z])#', '$1 ', strtolower($s));
+		return strtr(ucwords($s), ['/ ' => ':', '- ' => '']);
 	}
 
 }
